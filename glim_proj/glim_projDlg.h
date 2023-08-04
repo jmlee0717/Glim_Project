7c1e726303c7b6c354592148df4774d19cf1303b
jmlee <jm0717@hanmai.net>
2023-08-04 11:43:32 +0900

    <<< glim project file upload
    <<< 작업 사항
    1. 램덤하게 원을 그리는 루틴 구현
    2. 중심좌표를 구하여 십자표시로 표출
    3. 원 바깥 외각선 테두리를 노란색으로 색칠

---
+++ glim_proj/glim_projDlg.h
@@ -0,0 +1,48 @@
+﻿
+// glim_projDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CglimprojDlg 대화 상자
+class CglimprojDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CglimprojDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_GLIM_PROJ_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedDrawBtn();
+
+private:
+	void drawData(CDC* pDC);
+	void drawCenter(CDC* pDC);
+	void SetRectInit();
+
+	CImage	m_image;
+	int m_nRadius;
+	CPoint m_CenterPt;
+	int m_nPointx, m_nPointy;
+	CStatic	m_GroundStatic;
+	BOOL m_bDrawFlag = FALSE;
+};
